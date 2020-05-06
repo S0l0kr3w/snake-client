4b9a3d3c04191ce8f6fdf6434c3fba2c98b4639a
Jason Junio <jjjunio@gmail.com>
2020-05-06 17:15:03 +0000

    created play.js file

---
+++ play.js
@@ -0,0 +1,25 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '135.23.222.148',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+    conn.on('connect', () => {
+      conn.write('Hello from client!');
+  });             
+  
+  });
+  conn.setEncoding('utf8'); 
+  
+  return conn;
+}
+
+console.log('Connecting ...');
+connect();
