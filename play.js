13b702c0d141b7da42ec99c840ab00c61acf2ee3
Jason Junio <jjjunio@gmail.com>
2020-05-06 17:45:12 +0000

    refactored play.js to import client.js

--- play.js
+++ play.js
@@ -1,25 +1,5 @@
-const net = require('net');
 
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '135.23.222.148',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-    conn.on('connect', () => {
-      conn.write('Hello from client!');
-  });             
-  
-  });
-  conn.setEncoding('utf8'); 
-  
-  return conn;
-}
+const { connect } = require('./client');
 
 console.log('Connecting ...');
 connect();
